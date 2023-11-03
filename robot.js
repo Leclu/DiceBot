79e8c171aabe0d53ab95196786389ab2d5c1764f
Leclu <57877797+Leclu@users.noreply.github.com>
2023-11-04 00:37:08 +0100

    Update robot.js

--- robot.js
+++ robot.js
@@ -13,7 +13,7 @@ const commands = [
     .setDescription('Roll the dice!')
     .addIntegerOption(option =>
       option
-      .setName('num_dice')
+      .setName('number of dice')
         .setDescription('Number of dice to roll (1 or 2)')
         .setRequired(true)
     )
@@ -60,7 +60,7 @@ client.on('interactionCreate', async (interaction) => {
   const { commandName, options } = interaction;
 
   if (commandName === 'roll') {
-    const numDice = options.getInteger('num_dice');
+    const numDice = options.getInteger('number of dice');
     const sides = options.getInteger('sides');
 
     if (numDice === 1 || numDice === 2) {
